cc2a5053aa3765b36999324397d7f1aef4c398e3
jhtong <me@joeltong.org>
2019-08-03 00:04:32 +0800

    added citi scraper to code

--- src/scrapers/mainScraper.js
+++ src/scrapers/mainScraper.js
@@ -1,14 +1,16 @@
+import logger from '../logger';
+
 import scrapeBurppleScheduled from './burppleScraper';
 import scrapeChopeScheduled from './chopeScraper';
 import scrapeAmexScheduled from './amexScraper';
-
-import logger from '../logger';
+import scrapeCitiScheduled from './citiScraper';
 
 const startScraping = () => {
   logger.info('== Starting scrapers ==');
   scrapeBurppleScheduled('1 2 */1 * *');
   scrapeChopeScheduled('1 2 */1 * *');
   scrapeAmexScheduled('1 2 */1 * *');
+  scrapeCitiScheduled('1 2 */1 * *');
 };
 
 export default startScraping;
